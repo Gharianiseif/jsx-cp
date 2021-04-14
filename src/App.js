07958670363f13e3f2e5e918c17904c60334c189
seif <HP@DL-LP-019.dlines.local>
2021-04-14 14:37:35 +0100

    jsx cd

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import imgSource from "./imageInSrc.jpg";
+import "./App.css";
+import "./Style.css";
+
+function App() {
+  let name = "Gharianiseif";
+  return (
+    <div>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">{name}</h1>
+
+        <br />
+        <img src={imgSource} alt="imageInSrc" />
+        <br />
+        <img src="/imageInPublic.jpg" alt="imageInPublic" />
+      </div>
+      <video width={320} height={240} controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
